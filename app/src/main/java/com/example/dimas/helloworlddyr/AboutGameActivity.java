e09c1e5034e724135119c14429ed645d03f29626
Dimas YR <dimas.yoan.r@gmail.com>
2017-12-21 07:38:54 +0700

    about acticvty

---
+++ app/src/main/java/com/example/dimas/helloworlddyr/AboutGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.dimas.helloworlddyr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_game);
+    }
+}
